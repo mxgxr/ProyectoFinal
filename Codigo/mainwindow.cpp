02182d6f4e3093c0b146a4bc347a99372c9d3803
mxgxr <manuela.gutierrezr@udea.edu.co>
2021-10-21 00:24:17 -0500

    Avance gui

--- Codigo/mainwindow.cpp
+++ Codigo/mainwindow.cpp
@@ -7,12 +7,15 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     setWindowTitle("The master key");
-    scene1 = new QGraphicsScene();
-    ui->graphicsView->setScene(scene1);
-    scene1->setSceneRect(-600,-300,1200,600);
-    letrero2 = new QImage("images/letrero2.jpg");
+    ePrincipal();
+
+
+
+
+  /* scene1->setSceneRect(-600,-300,1200,600);
+    letrero2 = new QImage("../images/letrero2.jpg");
     scene1->addPixmap(QPixmap::fromImage(*letrero2));
-  /*  scene1 = new QGraphicsScene();
+    scene1 = new QGraphicsScene();
     ui->graphicsView->setScene(scene1);
     scene1->setSceneRect(-600,-300,1200,600);
     pared1 = new Pared(0,0);
@@ -22,5 +25,25 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::ePrincipal()
+{
+    scene1 = new QGraphicsScene();
+    ui->graphicsView->setScene(scene1);
+    ui->graphicsView->show();
+    ui->graphicsView->setSceneRect(0,0,1000,600);
+
+    letrero2 = new QImage("../images/letrero2.jpg");
+    scene1->addPixmap(QPixmap::fromImage(*letrero2));
+    registrar = new QPushButton("Registrar",this);
+    registrar->setGeometry(200,300,300,100);
+    //registrar->setGeometry(QRect(QPoint(0,100), QSize(300,100)));
+    scene1->addWidget(registrar);
+    ingresar = new QPushButton("Ingresar",this);
+    ingresar->setGeometry(QRect(QPoint(600,300), QSize(300,100)));
+    scene1->addWidget(ingresar);
+    title1 = new QLabel("Welcome to \n The master key",this);
+    title1->setGeometry(400,100,300,100);
 }
 
--- Codigo/mainwindow.h
+++ Codigo/mainwindow.h
@@ -4,6 +4,10 @@
 #include <QMainWindow>
 #include <QGraphicsScene>
 #include <QImage>
+#include <QLabel>
+#include <QPushButton>
+#include <QLineEdit>
+
 #include "pared.h"
 
 QT_BEGIN_NAMESPACE
@@ -17,11 +21,22 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void ePrincipal();
+    void eRegistro();
+    void eInicio();
+    void level1();
+    void level2();
+    void level3();
+
+
+private slots:
 
 private:
     QGraphicsScene *scene1, *scene2, *scena3, *scena4, *scena5, *scena6, *scene7, *scene8; //1. inicio(ingresar/registrar) 2.ingresar 3.registrar 4.nivel1 5.nivel2 6.nivel3 7.continuar,salir 8.finalizado el juego
     QImage *letrero2;
-    //Pared *pared1;
+    QPushButton *registrar, *ingresar;
+    QLabel *title1;
+    Pared *pared1;
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H

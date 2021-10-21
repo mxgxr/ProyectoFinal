d8b0ffe740cfbf7ed43ba55b43eaf17e1056820e
mxgxr <manuela.gutierrezr@udea.edu.co>
2021-10-20 19:05:10 -0500

    Avance interfaz grafica

--- Codigo/mainwindow.cpp
+++ Codigo/mainwindow.cpp
@@ -6,12 +6,17 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    setWindowTitle("RobinHood");
+    setWindowTitle("The master key");
     scene1 = new QGraphicsScene();
     ui->graphicsView->setScene(scene1);
     scene1->setSceneRect(-600,-300,1200,600);
+    letrero2 = new QImage("images/letrero2.jpg");
+    scene1->addPixmap(QPixmap::fromImage(*letrero2));
+  /*  scene1 = new QGraphicsScene();
+    ui->graphicsView->setScene(scene1);
+    scene1->setSceneRect(-600,-300,1200,600);
     pared1 = new Pared(0,0);
-    scene1->addItem(pared1);
+    scene1->addItem(pared1);*/
 }
 
 MainWindow::~MainWindow()
--- Codigo/mainwindow.h
+++ Codigo/mainwindow.h
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include <QGraphicsScene>
+#include <QImage>
 #include "pared.h"
 
 QT_BEGIN_NAMESPACE
@@ -19,7 +20,8 @@ public:
 
 private:
     QGraphicsScene *scene1, *scene2, *scena3, *scena4, *scena5, *scena6, *scene7, *scene8; //1. inicio(ingresar/registrar) 2.ingresar 3.registrar 4.nivel1 5.nivel2 6.nivel3 7.continuar,salir 8.finalizado el juego
-    Pared *pared1;
+    QImage *letrero2;
+    //Pared *pared1;
     Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H

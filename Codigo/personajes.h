ce92f1435584db10d75a3f535311fa664f772b79
Danielagd <daniela.gallegod@udea.edu.co>
2021-10-21 21:56:04 -0500

    Sprites listos, clase adelantada

---
+++ Codigo/personajes.h
@@ -0,0 +1,21 @@
+#ifndef PERSONAJES_H
+#define PERSONAJES_H
+
+#include <QGraphicsItem>
+#include <QGraphicsScene>
+#include <QPainter>
+#include <QPixmap>
+
+class personajes:public QGraphicsItem
+{
+public:
+    int posx,posy;
+    QPixmap personajebrush;
+    personajes(int,int,QPixmap);
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = nullptr);
+protected:
+
+};
+
+#endif // PERSONAJES_H

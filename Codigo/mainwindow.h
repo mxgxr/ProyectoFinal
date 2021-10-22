141456ae11732f6eef583bec80847527f2a99311
Danielagd <daniela.gallegod@udea.edu.co>
2021-10-21 21:53:08 -0500

    Sprites listos, clase personajes adelantada

--- Codigo/mainwindow.h
+++ Codigo/mainwindow.h
@@ -3,8 +3,12 @@
 
 #include <QMainWindow>
 #include <QGraphicsScene>
+#include <QPixmap>
 #include "pared.h"
+#include "personajes.h"
 
+#define jugador_1 ":/jugadores/sprites/1personaje.png"
+#define jugador_2 ":/jugadores/sprites/2personaje.png"
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -18,8 +22,9 @@ public:
     ~MainWindow();
 
 private:
-    QGraphicsScene *scene1;
     Pared *pared1;
+    personajes *jugador1,*jugador2;
     Ui::MainWindow *ui;
+    QGraphicsScene *scene1;
 };
 #endif // MAINWINDOW_H
